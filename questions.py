11b5064ea99d823d7796b7f47b5c035ed91dfe48
Cyberdrey <74965281+cyberdreyy@users.noreply.github.com>
2026-08-17 23:14:43 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,8 +2,8 @@ import json
 import os
 
 MAX_REPO = 25
-SOURCE_REPO = "0dotxyz/marginfi-v2"
-REPO_NAME = "marginfi-v2"
+SOURCE_REPO = "codertjay/privy-js-sdk-core-audit"
+REPO_NAME = "privy-js-sdk-core-audit"
 TREE = ""
 BRANCH = ""
 # Example:
