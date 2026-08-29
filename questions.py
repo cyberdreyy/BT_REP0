1f9222c7c90f24beb5e27f00a820bd0ad165c200
Cyberdrey <74965281+cyberdreyy@users.noreply.github.com>
2026-08-29 09:49:59 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,8 +2,8 @@ import json
 import os
 
 MAX_REPO = 25
-SOURCE_REPO = "smartcontractkit/chainlink"
-REPO_NAME = "chainlink"
+SOURCE_REPO = "Zest-Protocol/zest-v2-contracts"
+REPO_NAME = "zest-v2-contracts"
 TREE = ""
 BRANCH = ""
 # Example:
