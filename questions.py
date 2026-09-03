b4cc7169f31e1ec1b640ee84012be15aea7ee502
Cyberdrey <74965281+cyberdreyy@users.noreply.github.com>
2026-09-03 14:57:05 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,8 +2,8 @@ import json
 import os
 
 MAX_REPO = 25
-SOURCE_REPO = "near/core-contracts"
-REPO_NAME = "core-contracts"
+SOURCE_REPO = "stacks-network/stacks-core"
+REPO_NAME = "stacks-core"
 TREE = ""
 BRANCH = ""
 # Example:
